3a0f296817b67120b2d87d737d2c96e6f0ca1524
Midson <bruno_campos2012@outlook.com>
2022-12-25 23:05:51 -0300

    Fucking Stacks

---
+++ BlackSilence Rebalance Project/Bufs/FuriosoIcon.cs
@@ -0,0 +1,23 @@
+﻿using BigDLL4221.Buffs;
+
+namespace BlackSilence_Rebalance_Project.Bufs
+{
+    public class BattleDiceCardBuf_BSRebalanceEgoCount_md5488 : BattleDiceCardBuf
+    {
+        protected override string keywordIconId => "FuriosoIcon_md5488";
+    }
+
+    //Buff that count the used cards
+    public class BattleUnitBuf_BSRebalanceSpecialCount_md5488 : BattleUnitBuf_BaseBufChanged_DLL4221
+    {
+        public BattleUnitBuf_BSRebalanceSpecialCount_md5488() : base(infinite: true, lastOneScene: false)
+        {
+
+        }
+
+          public override string BufName => "Furioso";
+          public override string bufActivatedText => $"Number of the Black Silence's Combat Pages used : {stack}";
+        //protected override string keywordIconId => "WhiteNoiseCard_md5488";
+        // protected override string keywordId => "SpecialCount_md5488";
+    }
+}
--- BlackSilence Rebalance Project/Passives/CardBuffsPassive.cs
+++ BlackSilence Rebalance Project/Passives/CardBuffsPassive.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using BigDLL4221.Utils;
-using UnityEngine;
+using BlackSilence_Rebalance_Project.Bufs;
 
 namespace BlackSilence_Rebalance_Project.Passives
 {
     public class PassiveAbility_CardBuffsPassive_md5488 : PassiveAbilityBase
     {
+        private readonly List<LorId> _usedCount = new List<LorId>();
         public override void OnDrawCard()
         {
             var buffToGive = new List<Type>();
@@ -27,5 +28,39 @@ namespace BlackSilence_Rebalance_Project.Passives
                 SingletonBehavior<BattleManagerUI>.Instance.ui_unitListInfoSummary.UpdateCharacterProfile(unit, unit.faction, unit.hp, unit.breakDetail.breakGauge);
             }
         }
+        public override void OnWaveStart()
+        {
+            owner.personalEgoDetail.AddCard(BSRebalanceModParameters.FuriosoCard);
+        }
+
+        public override void OnRoundStart()
+        {
+            foreach (var battleDiceCardModel in owner.allyCardDetail.GetAllDeck())
+            {
+                battleDiceCardModel.RemoveBuf<BattleDiceCardBuf_BSRebalanceEgoCount_md5488>();
+                var lorId = battleDiceCardModel.GetID();
+                if (lorId.packageId != BSRebalanceModParameters.PackageId) continue;
+                if (!_usedCount.Contains(lorId) && BSRebalanceModParameters.BSRebalanceCards.Contains(lorId.id))
+                    battleDiceCardModel.AddBuf(new BattleDiceCardBuf_BSRebalanceEgoCount_md5488());
+            }
+
+            owner.bufListDetail.RemoveBufAll(typeof(BattleDiceCardBuf_BSRebalanceEgoCount_md5488));
+            owner.bufListDetail.AddBuf(new BattleDiceCardBuf_BSRebalanceEgoCount_md5488
+            {
+                stack = _usedCount.Count
+            });
+        }
+
+        public bool IsActivatedSpecialCard()
+        {
+            return _usedCount.Count >= 9;
+        }
+
+        public void ResetUsedCount()
+        {
+            _usedCount.Clear();
+        }
+
+        
     }
 }
